e756d607cb65ab9f49d698e45d9962a6087467de
Ramiro Meares <ramiro.meares@ampush.com>
2015-10-26 17:25:30 -0700

    Merged client and server

--- chat/main.cpp
+++ chat/main.cpp
@@ -6,53 +6,98 @@
 //  Copyright © 2015 Ramiro Meares. All rights reserved.
 //
 
+#include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <netdb.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include "port.h"
+#include <thread>
 
 #define BUFSIZE 2048
+#define SERVICE_PORT    21234
+
+
+void recieve_messages(int socket_descriptor, sockaddr_in remote_address, socklen_t addresses_len){
+    int recvlen;			/* # bytes received */
+    char buf[BUFSIZE];      /* receive buffer */
+    for (;;) {
+        recvlen = (int)recvfrom(socket_descriptor, buf, BUFSIZE, 0, (struct sockaddr *)&remote_address, &addresses_len);
+        if (recvlen > 0) {
+            buf[recvlen] = 0;
+            printf("Peer: %s\n", buf);
+        }
+        else
+            printf("ERROR: Recieved invalid message\n");
+    }
+}
+
 
 int main(int argc, const char * argv[]) {
     struct sockaddr_in my_address, remote_address;
     socklen_t addresses_len = sizeof(remote_address);
-    int recvlen;			/* # bytes received */
-    int msg_cnt = 0;
-    char buf[BUFSIZE];	/* receive buffer */
-    int fd; /* socket */
+    int socket_descriptor;
+    int service_port;
+    char *server;
+    bool is_server;
     
-    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("cannot create socket\n");
+    if (argc < 1) {
+        service_port = SERVICE_PORT;
+        is_server = true;
+    } else if (argc < 2) {
+        service_port = atoi(argv[0]);
+        is_server = true;
+    } else {
+        service_port = atoi(argv[0]);
+        strcpy(server, argv[1]);
+        is_server = false;
+    }
+    
+    if ((socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("ERROR: Cannot create socket\n");
         return 0;
     }
     
     memset((char *)&my_address, 0, sizeof(my_address));
     my_address.sin_family = AF_INET;
     my_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    my_address.sin_port = htons(SERVICE_PORT);
-    
-    if (bind(fd, (struct sockaddr *)&my_address, sizeof(my_address)) < 0) {
-        perror("bind failed\n");
+    if (is_server){
+        my_address.sin_port = htons(service_port);
+    }else{
+        my_address.sin_port = htons(0);
+    }
+
+
+    if (bind(socket_descriptor, (struct sockaddr *)&my_address, sizeof(my_address)) < 0) {
+        perror("ERROR: Bind failed\n");
         return 0;
     }
     
-    for (;;) {
-        printf("waiting on port %d\n", SERVICE_PORT);
-        recvlen = (int)recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remote_address, &addresses_len);
-        if (recvlen > 0) {
-            buf[recvlen] = 0;
-            printf("received message: \"%s\" (%d bytes)\n", buf, recvlen);
+    if (!is_server){
+        memset((char *) &remote_address, 0, sizeof(remote_address));
+        remote_address.sin_family = AF_INET;
+        remote_address.sin_port = htons(service_port);
+        if (inet_aton(server, &remote_address.sin_addr)==0) {
+            perror("ERROR: inet_aton() failed\n");
+            return 0;
         }
-        else
-            printf("uh oh - something went wrong!\n");
-        sprintf(buf, "ack %d", msg_cnt++);
-        printf("sending response \"%s\"\n", buf);
-        if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remote_address, addresses_len) < 0)
-            perror("sendto");
+        printf("Conntected to server %s in port %d\n", server, service_port);
+    }else{
+        printf("Created server in port %d\n", service_port);        
+    }
+    
+    std::thread t1(recieve_messages, socket_descriptor, remote_address, addresses_len);
+    
+    char buf[BUFSIZE];      /* send buffer */
+    for (;;) {
+        std::cin >> buf;
+        printf("Me: %s\n", buf);
+        if (sendto(socket_descriptor, buf, strlen(buf), 0, (struct sockaddr *)&remote_address, addresses_len) < 0)
+            perror("ERROR: sendto() failed\n");
+        
     }
     
     return 0;
 }
+
--- chat/port.h
+++
@@ -1,9 +0,0 @@
-//
-//  port.h
-//
-//  Created by Ramiro Meares on 10/25/15.
-//  Copyright © 2015 Ramiro Meares. All rights reserved.
-//
-
-#define SERVICE_PORT	21234	/* hard-coded port number */
-
